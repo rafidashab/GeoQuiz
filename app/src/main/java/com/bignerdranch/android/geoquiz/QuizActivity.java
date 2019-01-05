fee79e17cf45c505e8111b5223fb2c7169a5586c
rafidashab <rafidashabpranta@gmail.com>
2019-01-05 00:19:40 -0800

    Chapter 1 Review Done

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,12 +2,38 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-    }
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+
+        // Listener Interface needs to be implemented when we need to create one instance of an object
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, R.string.true_button, Toast.LENGTH_SHORT).show();
+                //Mistake 2: Forgot to put show() at the end of the toast
+            }
+        });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Toast.makeText(QuizActivity.this, R.string.false_button , Toast.LENGTH_SHORT).show();
+                }
+        });
+
+    }  // Mistake 1 : Putting the button references outside the onCreate function
 }
